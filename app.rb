3ca7f07f31add1ab48d7a6546c19c03ed669ed30
Omar Branez <omar.branez@gmail.com>
2021-04-19 11:30:48 -0400

    commit tree 76c13277b7ac2d89a9d631f9c60e4ec7102a5ff4

--- app.rb
+++ app.rb
@@ -7,5 +7,12 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
+  post '/food' do
+    # params.to_s
+    # displays the inputted params (name and favorite_food) in the page specified in the /food route
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+    # displays as a normal english sentence string. note there's no database, just the params storage
+  end
+
 
 end
